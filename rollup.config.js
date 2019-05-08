d55343faf3a341c841001f4341e742fa843286de
bmcmahen <ben.mcmahen@gmail.com>
2019-05-07 21:17:33 -0700

    fix cjs build and remove umd for now

--- rollup.config.js
+++ rollup.config.js
@@ -1,47 +1,40 @@
 import resolve from "rollup-plugin-node-resolve";
 import filesize from "rollup-plugin-filesize";
-import { uglify } from "rollup-plugin-uglify";
 import pkg from "./package.json";
 import commonjs from "rollup-plugin-commonjs";
 import cleanup from "rollup-plugin-cleanup";
 import json from "rollup-plugin-json";
+import typescript from "rollup-plugin-typescript2";
 
-const commonjsOptions = {
-  ignoreGlobal: true,
-  include: /node_modules/
-};
-
-const input = pkg.main;
+const input = "src/index.ts";
 
 const plugins = [
   resolve(),
-  commonjs(commonjsOptions),
+  typescript({
+    typescript: require("typescript")
+  }),
+  commonjs(),
   json(),
   cleanup(),
-  uglify(),
   filesize()
 ];
 
-const globals = {
-  react: "React",
-  "react-doc": "ReactDOM"
-};
-
-const capitalize = s => {
-  if (typeof s !== "string") return "";
-  return s.charAt(0).toUpperCase() + s.slice(1);
-};
+const externals = [
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {})
+];
 
 export default [
   {
     input,
-    output: {
-      file: "umd/pan-responder-hook.js",
-      format: "umd",
-      name: capitalize("pan-responder-hook"),
-      globals
-    },
-    external: Object.keys(globals),
+    output: [
+      {
+        file: pkg.main,
+        format: "cjs",
+        sourcemap: true
+      }
+    ],
+    external: externals,
     plugins
   }
 ];
